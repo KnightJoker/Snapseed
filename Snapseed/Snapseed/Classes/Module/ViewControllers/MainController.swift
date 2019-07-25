f8ca03ba9b991a152ed41b32dd95167542f17091
KnightJoker <cbearxl@gmail.com>
2019-07-25 19:38:40 +0800

    [Feature] Photo selection

--- Snapseed/Snapseed/Classes/Module/ViewControllers/MainController.swift
+++ Snapseed/Snapseed/Classes/Module/ViewControllers/MainController.swift
@@ -12,13 +12,15 @@ class MainController: UIViewController {
 
     @IBOutlet weak var moreButton: UIButton!
     @IBOutlet weak var addImageView: UIImageView!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
     
     private var imagePickerController: UIImagePickerController? {
         get {
             _imagePickerController = UIImagePickerController()
             _imagePickerController?.allowsEditing = true
             _imagePickerController?.sourceType = .photoLibrary
-            _imagePickerController?.delegate = self as? UIImagePickerControllerDelegate & UINavigationControllerDelegate
+            _imagePickerController?.delegate = self
             return _imagePickerController
         }
         set {
@@ -48,13 +50,29 @@ class MainController: UIViewController {
         addImageView.image = (UIImage(named: "ic_add_green")?.withRenderingMode(UIImage.RenderingMode.alwaysTemplate))
         addImageView.tintColor = kThemeBlackColor
         
+        imageView.isHidden = true
+    }
+    
+    private func setupImageView(_ image:UIImage) {
+        addImageView.isHidden = true
+        tipLabel.isHidden = true
+        imageView.isHidden = false
+        imageView?.image = image
     }
 
 }
 
-extension MainController : UIImagePickerControllerDelegate {
+extension MainController : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        
+        if let image = info[.originalImage] as? UIImage {
+            setupImageView(image)
+            dismiss(animated: true, completion: nil)
+        }
     }
     
 }
---
+++ Snapseed/Snapseed/Classes/Module/Views/MenuView.swift
@@ -0,0 +1,21 @@
+//
+//  MenuView.swift
+//  Snapseed
+//
+//  Created by Xuli Chen on 2019/7/25.
+//  Copyright © 2019 Huni. All rights reserved.
+//
+
+import UIKit
+
+class MenuView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
